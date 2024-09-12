b4778cc3e763d1f61859365605502260d001ccae
Alex <125302863+alexky0@users.noreply.github.com>
2024-09-12 14:25:17 -0400

    did something or other

--- main.py
+++ main.py
@@ -1,60 +1,17 @@
-import os
-
-#Task 4
-def add(x, y):
-    return x + y
-
-#Task 5
-def calculate(x, y, operation="equal"):
-    if (operation == "add"):
-        return x + y
-    elif (operation == "subtract"):
-        return x - y
-    elif (operation == "multiply"):
-        return x * y
-    elif (operation == "divide"):
-        return x / y
-    elif (operation == "equal"):
-        return x == y
-    else:
-        return "No operation"
-
+def bake_cookie(ingredients, instructions, temperature, cutter="heart",  for_sarslols=True, name="haley"):
+    print("ingredirents")
+    for ingredient in ingredients:
+        print(ingredient)
+    print("insterucshins", end="\n")
+    print(instructions)
+    print(f"The oven is at {temperature} degreez")
+    print(f"Shape: {cutter}")   
+        
 def main():
-    print(__name__)
-    print("hello world")
+    ingredients = ["flour", "sugar", "eggs", "butter", "chocolate chips", "cinnamon"]
+    instructions = "mix and bake"
+    temp = 300
+    bake_cookie(ingredients, instructions, temp, "omar")
     
-    #Task 1
-    x = 42
-    y = 3/4
-    z = int('7')
-    a = float(5)
-    name = "Alex"
-    print(type(x), '\n', type(y), '\n', type(z), '\n', type(a), '\n', type(name))
-
-    #Task 2
-    rent = 480
-    per_day = rent/30
-    print(per_day)
-
-    #Task 3
-    print("Hello world")
-    print(f"My name is {name}")
-
-    #Task 4
-    print(add(x, x))
-
-    #Task 5
-    print(calculate(x, y, "equal"))
-
-    num = 5
-    number = open("number.txt", 'w')
-    if (os.stat("number.txt").st_size == 0):
-        number.write("5")
-    number = open("number.txt", 'r')
-    val = int(number.read())
-    number = open("number.txt", 'w')
-    number.write(str(val + num))
-    number.close()
-
 if __name__ == "__main__":
     main()

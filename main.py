cc823fc005da61829dc98c44c2fd2254f248e72d
Alex <125302863+alexky0@users.noreply.github.com>
2024-09-13 10:34:56 -0400

    calculate function, list iteration

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate(x, y, operation="equals"):
+def calculate(x, y, operation="add"):
     if (operation=="add"):
         return x+y
     elif (operation=="subtract"):
@@ -12,8 +12,21 @@ def calculate(x, y, operation="equals"):
     else:
         return "Invalid operation"
 
+def list_iteration(other_list):
+    new_list = []
+    for item in other_list:
+        new_list.append(item*2)
+    print(new_list)
+
+    other_list = [item * 2 for item in other_list]
+    print(other_list)
+
 def main():
-    print(calculate(5, 5))
+    print(calculate(2, 3))
+    print(calculate(2, 3, "subtract"))
+    print(calculate(2, 3, operation="subtract"))
+
+    list_iteration([5, 2, 3, 4])
 
 if __name__ == "__main__":
     main()

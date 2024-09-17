8a75f93dbce425e6044c72a56f3797dc24f71994
Alex <125302863+alexky0@users.noreply.github.com>
2024-09-17 15:09:32 -0400

    sets and  tuples

--- main.py
+++ main.py
@@ -11,12 +11,26 @@ def list_demo():
     my_list.remove('l')
     my_list.insert(4, 'l')
     print(my_list)
+    print("!" in my_list)
+    my_list.sort()
+    print(my_list)
 
 def tuple_demo():
-    print("tuple demo")
+    person = ("Alex", 16, "Roosevelt Island")
+    name, age, neighborhood = person
+    print(person)
 
 def set_demo():
-    print("set demo")
+    my_set = set()
+    my_set = {1,2,3,4,6,5,7,8,9,10, 2}
+    my_set.add(7)
+    my_set.remove(4)
+    print(my_set)
+    my_set.add(4)
+    other_set = {2,4,6,8,10,12}
+    print(my_set.union(other_set))
+    print(my_set.intersection(other_set))
+    print(my_set.difference(other_set))
 
 def dict_demo():
     print("dict demo")
